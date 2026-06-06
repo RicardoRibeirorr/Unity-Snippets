844f210f10c57856696940c58aefed9673431d11
Ricardo Ribeiro <ricardoribeiro.rr@outlook.com>
2026-06-06 17:45:58 +0100

    Create DisableAttribute for Unity Inspector
    Adds a custom PropertyAttribute to disable GUI for properties.

---
+++ Inspector Snippets/Disable Field/DisableAttribute.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+
+/*******************************************************
+ * 
+ *  File:       DisableAttribute.cs
+ *  Description: Disables the GUI for the decorated property in the Unity Inspector.
+ *  
+ *  Folder:     Assets/Scripts/<any folder except Editor>
+ *  
+ *  Author:     RicardoRibeiroRR
+ *  
+ *******************************************************/
+public class DisableAttribute : PropertyAttribute
+{
+}
